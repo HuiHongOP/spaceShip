19f79ac27136844928943f0193bc8982071ef79a
jason <hzheng130@gmail.com>
2022-09-17 12:29:52 -0400

    added connection between Backend and frontend test

--- server/express.js
+++ server/express.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api', (req, res) => {
+  res.json({"users":['userOne','userTwo','useThree']});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
