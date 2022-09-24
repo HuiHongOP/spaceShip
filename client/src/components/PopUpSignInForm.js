e9fc4dc958f1488ba650f3420ddfd1e959c312d5
Jamie Mei <jamiemei@Jamies-MBP.lan>
2022-09-24 00:27:07 -0400

    attempt at connecting sign in to backend

--- client/src/components/PopUpSignInForm.js
+++ client/src/components/PopUpSignInForm.js
@@ -1,16 +1,59 @@
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+
 const PopUpSignInForm = () =>{
+
+    const [username, setUser] = useState("");
+    const [password, setPwd] = useState("");
+    const [err, setErr] = useState("");
+    const [success, setSuccess] = useState(false);
+    let componentMounted = true;
+    
+    useEffect (() => {
+        setErr("");
+    },[username, password])
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        try {
+            const response = await axios.post("/login", 
+                JSON.stringify({username, password}), 
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+
+            )
+            const accessToken = response?.data?.accessToken;
+            localStorage.setItem("accessToken", accessToken);
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (error) {
+            if (!err?.response) {
+                setErr('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErr('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErr('Unauthorized');
+            } else {
+                setErr('Login Failed');
+            }
+        }
+    }
+
     return (
         <section className = "text-center m-3">
             <div>
                 <h2>Welcome to Sign in Form</h2>
-                <form action="/" method="get">
+                <form action="/" method="get" onSubmit={handleSubmit}>
                     <div>
                         <label for="name">Username</label>
-                        <input type="text" name="userName" placeholder="Input Username" required/>
+                        <input type="text" name="userName" placeholder="Input Username" onChange = { (e) => setUser(e.target.value)} required/>
                     </div>
                     <div>
                         <label for ="password">Password</label>
-                        <input type="text" name="password" placeholder="Input Password" required/>
+                        <input type="password" name="password" placeholder="Input Password" onChange = { (e) => setPwd(e.target.value)}  required/>
                     </div>
                     <div>
                         <input type="submit" value="Login"/>
