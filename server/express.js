2c8fdbd192ab07a226ae7c996522391d65438e98
danny-y17 <dy31003@gmail.com>
2022-09-26 01:07:03 -0400

    added ssl fixes

--- server/express.js
+++ server/express.js
@@ -7,7 +7,16 @@ const cartRoutes = require('./routes/cartRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 const port = process.env.PORT || 5000
 const cookieParser = require("cookie-parser");
+const compression = require('compression')
+const helmet = require('helmet')
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: isProduction ? 'https://spaceshipacc.herokuapp.com' : '*',
+}
 
+app.use(cors(origin))
+app.use(compression)
+app.use(helmet)
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(
