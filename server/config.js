8ec4aa6d5cea77fb0f52d60baba79761ed4e94ff
danny-y17 <dy31003@gmail.com>
2022-09-25 23:10:40 -0400

    profile added

--- server/config.js
+++ server/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 const { Pool } = require('pg')
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'spaceshipacc',
-  password: 'password',
+  user: 'iqaocotrhwuddw',
+  host: 'host=ec2-44-207-133-100.compute-1.amazonaws.com',
+  database: 'dcmm4i2a27tmi7',
+  password: 'c2dfea47878b15e67962c82160b447fb188b07763a27c070b297e8e163b5597b',
   port: 5432
 });
 /*const isProduction = process.env.NODE_ENV === 'production'
