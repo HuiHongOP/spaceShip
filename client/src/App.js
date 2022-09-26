7e2bdf89e1fbf1dacb9bad4daccb16141f48aa87
Danny <38337112+danny-y17@users.noreply.github.com>
2022-09-26 02:18:02 -0400

    Revert "Ready for heroku deployment"

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import React, {useEffect,useState} from "react";
+import NavBar from './components/NavBar';
+import PopUpSignInForm from "./components/PopUpSignInForm";
+import PopUpSignUpForm from './components/PopUpSignUpForm';
+import Banner from "./components/Banner";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+  useEffect(() =>{
+    fetch("/api").then(
+      response =>response.json()
+    ).then(
+      data =>{setBackendData(data)}
+    )
+  },[])
+  return (
+    <BrowserRouter>
+        <div className="App">
+        {/* {(typeof backendData.users === 'undefined') ? 
+        (<p>Loading...</p>): 
+        (backendData.users.map((user,i) =>(
+          <p key={i}>{user}</p>
+        )))
+        } */}
+          <NavBar/>
+          <div className = "content">
+            <Routes>
+              <Route exact path="/" element ={<><Banner/><Products/></>} >
+              </Route>
+              <Route exact path="/signIn" element ={<PopUpSignInForm/>} >
+              </Route>
+              <Route exact path="/signUp" element ={<PopUpSignUpForm/>} >
+              </Route>
+              <Route exact path="/checkCart" element ={<Cart/>} >
+              </Route>
+            </Routes>
+          </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
