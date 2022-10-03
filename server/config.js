fd49ad31ee868216bb4e4155c5a808842d7db04e
danny-y17 <dy31003@gmail.com>
2022-10-02 22:09:15 -0400

    fixes

--- server/config.js
+++ server/config.js
@@ -18,5 +18,6 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
+  rejectUnauthorized: false
 })
 module.exports = pool
