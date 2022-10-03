8898bff8c6b621fcfc083ef146daa3b39208d634
danny-y17 <dy31003@gmail.com>
2022-10-02 20:15:51 -0400

    fixes posting users

--- server/queries/usersQueries.js
+++ server/queries/usersQueries.js
@@ -41,7 +41,7 @@ const createUser = async (req,res) => {
         const {username,email} = req.body
         const password = req.body.password
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await pool.query('INSERT INTO users (username,email,password) VALUES ($1,$2,$3,$4) RETURNING *', [username,email,hashedPassword]);
+        const newUser = await pool.query('INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *', [username,email,hashedPassword]);
         res.json({users:newUser.rows[0]});
     }
     catch (error) {
